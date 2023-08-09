fdf429c9d42a0c53b9752c09a0a3a926ec7e3d89
hash-amit <hash.amitkumar@gmail.com>
2023-08-09 16:51:59 +0530

    Bind Gender radio button list

--- Cascading Dropdown/Cascading Dropdown/Registration.aspx.cs
+++ Cascading Dropdown/Cascading Dropdown/Registration.aspx.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Configuration;
+using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
@@ -15,7 +16,26 @@ namespace Cascading_Dropdown
         SqlConnection _connection = new SqlConnection(ConfigurationManager.ConnectionStrings["con_string"].ConnectionString);
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+                BindGender();
+            }
+        }
 
+        // Bind gender
+        public void BindGender()
+        {
+            _connection.Open();
+            SqlCommand cmd = new SqlCommand("spBindGender", _connection);
+            cmd.CommandType = CommandType.StoredProcedure;
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+            _connection.Close();
+            gender_rbl.DataValueField = "Gender ID";
+            gender_rbl.DataTextField = "GEN";
+            gender_rbl.DataSource = dt;
+            gender_rbl.DataBind();
         }
 
         protected void country_ddl_SelectedIndexChanged(object sender, EventArgs e)
