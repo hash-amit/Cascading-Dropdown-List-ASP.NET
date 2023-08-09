aa492c8c87e24fe8d7483bcaf17fa3a8fd552441
hash-amit <hash.amitkumar@gmail.com>
2023-08-09 15:41:19 +0530

    Create database connection string

--- Cascading Dropdown/Cascading Dropdown/Registration.aspx.cs
+++ Cascading Dropdown/Cascading Dropdown/Registration.aspx.cs
@@ -1,14 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
+
 namespace Cascading_Dropdown
 {
     public partial class Registration : System.Web.UI.Page
     {
+        SqlConnection _connection = new SqlConnection(ConfigurationManager.ConnectionStrings["con_string"].ConnectionString);
         protected void Page_Load(object sender, EventArgs e)
         {
 
